3f61829d2e27227f77df8417475ae18a1f10f18c
BunhengG <bbmc255@gmail.com>
2025-02-22 15:36:51 +0700

    Take Data to another file

---
+++ lib/core/config/Category.dart
@@ -0,0 +1,29 @@
+import 'AppStrings.dart';
+
+final List<Map<String, String>> categories = [
+  {
+    'title': AppStrings.categoriesPage.watch,
+    'value': 'watch',
+    'iconPath': 'assets/icon/icon_apple_watch.png'
+  },
+  {
+    'title': AppStrings.categoriesPage.cans,
+    'value': 'earphone',
+    'iconPath': 'assets/icon/icon_earphone.png'
+  },
+  {
+    'title': AppStrings.categoriesPage.charger,
+    'value': 'charger',
+    'iconPath': 'assets/icon/icon_charger.png'
+  },
+  {
+    'title': AppStrings.categoriesPage.phoneStands,
+    'value': 'phone_stand',
+    'iconPath': 'assets/icon/icon_phone_stand.png'
+  },
+  {
+    'title': AppStrings.categoriesPage.airPods,
+    'value': 'airpods',
+    'iconPath': 'assets/icon/icon_airpods.png'
+  },
+];
